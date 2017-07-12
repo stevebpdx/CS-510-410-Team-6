687d7b3533e849ad077f4f1b2e3b039fcf63e3fa
steve <stevebpdx@gmail.com>
2017-07-12 13:01:11 -0700

    Initial Commit

---
+++ SealTeam6/SealTeam6.Core/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealTeam6.Core
+{
+    public class Class1
+    {
+        public void stub()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
