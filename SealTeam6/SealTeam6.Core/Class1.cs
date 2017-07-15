d579ef1863600f8d33d8cb6a936274252e89be31
Patrick Overton <poverton@pdx.edu>
2017-07-14 21:26:22 -0700

    Added my attempt to implement the log in feature.

--- SealTeam6/SealTeam6.Core/Class1.cs
+++ SealTeam6/SealTeam6.Core/Class1.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SealTeam6.Core
 {
     public class Class1
     {
-        public void stub()
+        public static void LogIn()
         {
-            Console.WriteLine("Hello World");
+            Console.Write("Host: ");
+            String host = Console.ReadLine();
+            Regex regex = new Regex("\\A([a-zA-Z0-9]|\\.){11,}\\z");
+            while (!regex.IsMatch(host))
+            {
+                Console.Write("Invalid host. (Valid characters: letters, numbers and periods)\n");
+                Console.Write("Host: ");
+                host = Console.ReadLine();
+            }
+            Console.Write("Username: ");
+            String username = Console.ReadLine();
+            Console.Write("Password: ");
+            String password = "";
+            char key = Console.ReadKey(true).KeyChar;
+            while (key != (char)ConsoleKey.Enter)
+            {
+                password += key;
+                Console.Write("*");
+                key = Console.ReadKey(true).KeyChar;
+            }
+            Console.Write("\n");
+            Console.Write(password);
+            Console.ReadLine();
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + host + "/");
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+            request.Credentials = new NetworkCredential(username, password);
+            try
+            {
+                FtpWebResponse directoryListResponse = (FtpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                Console.Write(e.Message);
+            }
         }
     }
 }
