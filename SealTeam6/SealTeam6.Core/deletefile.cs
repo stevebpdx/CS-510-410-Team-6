a9932e62e5985a9c6283c29035dc396b801f2a69
vmannn <vicochia@pdx.edu>
2017-07-24 12:44:22 -0700

    Add files via upload

---
+++ SealTeam6/SealTeam6.Core/deletefile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SealTeam6.Core
+{
+    public class class2
+    {
+        public static bool delete_file(Uri file_to_delete)
+        {
+
+            if (file_to_delete.Scheme != Uri.UriSchemeFtp)
+            {
+                Console.Write("Not a valid file representation");
+                return false;
+
+            }
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(file_to_delete);
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+
+            try
+            {
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            }
+
+            catch(WebException m)
+            {
+
+                Console.Write(m.message);
+
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
