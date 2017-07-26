549771ff740e26e49a78ccdbb4861e8bab5d9550
steve <stevebpdx@gmail.com>
2017-07-25 19:53:49 -0700

    committing to db branch

---
+++ SealTeam6/SealTeam6.DataProvider/DataProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealTeam6.DataProvider
+{
+    public class DataProvider
+    {
+        public string ConnectStringName;
+
+        public DataProvider()
+        { }
+
+        public DataProvider(string connectStringName)
+        {
+            this.ConnectStringName = connectStringName;
+        }
+
+        public void SaveOrUpdateConnection(Connection connection)
+        {
+            using (var db = new SealTeam6Entities())
+            {
+                var result = db.Connections.FirstOrDefault(c => c.UserID == connection.UserID && c.Uri == connection.Uri);
+                if (result != null)
+                {
+                    result.UserID = connection.UserID;
+                    result.Uri = connection.Uri;
+                    result.Password = connection.Password;
+                    result.Directory = connection.Directory;
+
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Connections.Add(connection);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+
+
+    }
+}
