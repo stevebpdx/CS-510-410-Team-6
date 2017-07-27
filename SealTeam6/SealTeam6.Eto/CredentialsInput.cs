39d68739caac13a37815eff4d995fe06b82537b5
Barend Venter <barendventer@gmail.com>
2017-07-27 00:19:37 -0700

    Hookups for logging into server

---
+++ SealTeam6/SealTeam6.Eto/CredentialsInput.cs
@@ -0,0 +1,40 @@
+﻿using Eto.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealTeam6.EtoGUI
+{
+    class CredentialsInput
+    {
+        protected LabelledTextInput Username;
+        protected PasswordField Password;
+        public Control View { get; protected set; }
+        public NetworkCredential Credential
+        {
+            get
+            {
+                return new NetworkCredential(Username.Text, Password.Text);
+            }
+            set
+            {
+                Username.Text = value.UserName;
+                Password.Text = value.Password;
+            }
+        }
+
+        public CredentialsInput()
+        {
+            Username = new LabelledTextInput("Username:");
+            Password = new PasswordField();
+            View = new StackLayout();
+            var view = (StackLayout)View;
+            view.Items.Add(Username.View);
+            view.Items.Add(Password.View);
+            view.Orientation = Orientation.Vertical;
+        }
+    }
+}
---
+++ SealTeam6/SealTeam6.Eto/ServerLoginForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Eto.Forms;
+using FluentFTP;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealTeam6.EtoGUI
+{
+    class ServerLoginForm
+    {
+        protected CredentialsInput _credentials;
+        protected LabelledTextInput _hostName;
+        protected Button _submit;
+        public event EventHandler<FtpClient> SubmitButtonPressed;
+        public Control View { get; protected set; }
+        
+        public FtpClient StartSession()
+        {
+            return new FtpClient(_hostName.Text, _credentials.Credential);
+        }
+
+        protected void submit()
+        {
+            if(SubmitButtonPressed != null) SubmitButtonPressed.Invoke(this, StartSession());
+        }
+
+        public ServerLoginForm()
+        {
+            _submit = new Button((s,e) => submit());
+            _submit.Text = "Login";
+            _credentials = new CredentialsInput();
+            _hostName = new LabelledTextInput("Hostname:");
+            View = new StackLayout();
+            var view = (StackLayout)View;
+            view.Items.Add(_hostName.View);
+            view.Items.Add(_credentials.View);
+            view.Items.Add(_submit);
+            view.Orientation = Orientation.Vertical;
+        }
+    }
+}
