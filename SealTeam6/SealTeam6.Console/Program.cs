761cd9c5371a5f5c9756b4693e92fca60fc84f0b
Patrick Overton <poverton@pdx.edu>
2017-08-06 21:02:02 -0700

    Added a Fluent-friendly implementation for the ListRemote feature.

--- SealTeam6/SealTeam6.Console/Program.cs
+++ SealTeam6/SealTeam6.Console/Program.cs
@@ -6,6 +6,59 @@ namespace SealTeam6.Console
 {
     class Program
     {
+        public static void ListRemote(FluentFTP.FtpClient session, String directory)
+        {
+            FluentFTP.FtpListItem[] list = session.GetListing(directory);
+            System.Console.WriteLine("Date       Time     <DIR> or Bytes Name");
+            foreach (var item in list)
+            {
+                System.Console.Write(item.Modified.GetDateTimeFormats()[56] + " ");
+                if (item.Type == FluentFTP.FtpFileSystemObjectType.Directory)
+                {
+                    System.Console.Write("<DIR>          ");
+                }
+                else
+                {
+                    String size = item.Size.ToString();
+                    String spaces = new String(' ', (14 - size.Length));
+                    System.Console.Write(spaces + size + " ");
+                }
+                System.Console.WriteLine(item.Name);
+            }
+        }
+
+        public static String PromptDirectory(FluentFTP.FtpClient session, bool enforce)
+        {
+            System.Console.Write("Directory: ");
+            String directory = System.Console.ReadLine();
+            if (enforce)
+            {
+                while (directory == null || !session.DirectoryExists(directory))
+                {
+                    System.Console.WriteLine("Invalid directory.");
+                    System.Console.Write("Directory: ");
+                    directory = System.Console.ReadLine();
+                }
+            }
+            return directory;
+        }
+
+        public static String PromptFile(FluentFTP.FtpClient session, bool enforce)
+        {
+            System.Console.Write("File: ");
+            String file = System.Console.ReadLine();
+            if (enforce)
+            {
+                while (file == null || !session.FileExists(file))
+                {
+                    System.Console.WriteLine("Invalid file.");
+                    System.Console.Write("File: ");
+                    file = System.Console.ReadLine();
+                }
+            }
+            return file;
+        }
+
         public static String PromptHost()
         {
             System.Console.Write("Host: ");
@@ -64,6 +117,8 @@ namespace SealTeam6.Console
             String username = PromptString("Username", true);
             String password = PromptPassword();
             var session = Class1.LogIn(host, username, password);
+            String directory = PromptDirectory(session, true);
+            ListRemote(session, directory);
             Class1.LogOut(session);
             System.Console.ReadLine();
         }
