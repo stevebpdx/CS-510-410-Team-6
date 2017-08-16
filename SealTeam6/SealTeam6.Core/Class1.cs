e81909af5ce5fd0b9c6c5e8afab4906d2de88188
vmannn <vicochia@pdx.edu>
2017-08-16 14:26:19 -0700

    Add files via upload

--- SealTeam6/SealTeam6.Core/Class1.cs
+++ SealTeam6/SealTeam6.Core/Class1.cs
@@ -7,6 +7,151 @@ namespace SealTeam6.Core
 {
     public class Class1
     {
+           
+
+        public static void PutFile(FluentFTP.FtpClient session, String local, String remote)
+        {
+
+            try{
+
+                session.UploadFile(local, remote);
+
+            }
+
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: " + e.Message);
+			}
+
+        }
+
+		public static void PutFiles(FluentFTP.FtpClient session, String directory, List<String> files)
+		{
+			try
+			{
+                session.UploadFiles(files, directory);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception: " + e.Message);
+			}
+		}
+
+
+
+        public static bool ChangeFileName(FluentFTP.FtpClient fluentSession){
+
+
+            System.Console.WriteLine("Please provide file name you would like to change:");
+            string to_change = System.Console.ReadLine();
+
+            if (!FileExists(fluentSession, to_change))
+                return false;
+
+
+            System.Console.WriteLine("Please enter what you would like to change your file name to:");
+            string new_name = System.Console.ReadLine();
+
+            fluentSession.Rename(to_change, new_name);
+
+            return true;
+
+        }
+        
+
+        public static bool DirectoryExists(FluentFTP.FtpClient fluentSession, string file_name)
+        {
+
+            try
+            {
+                fluentSession.DirectoryExists(file_name);
+                return true;
+            }
+
+            catch (WebException e)
+            {
+                System.Console.WriteLine();
+                System.Console.Write(e.Message);
+            }
+
+
+            return false;
+        }
+
+
+		public static bool FileExists(FluentFTP.FtpClient fluentSession, string file_name)
+		{
+            
+			try
+			{
+				fluentSession.FileExists(file_name);
+				return true;
+			}
+
+			catch (WebException e)
+			{
+
+				System.Console.WriteLine();
+				System.Console.Write(e.Message);
+			}
+
+			return false;
+		}
+
+
+        public static string DeletesDirectory(FluentFTP.FtpClient fluentSession)
+        {
+            
+			System.Console.WriteLine("Please provide name of Directory you would like to delete");
+			string directory_name = System.Console.ReadLine();
+
+            //function to check if directory exists
+            DirectoryExists(fluentSession, directory_name);
+
+			try
+			{
+                fluentSession.DeleteDirectory(directory_name);
+                return directory_name;
+			}
+
+			catch (WebException e)
+			{
+
+				System.Console.WriteLine();
+				System.Console.Write(e.Message);
+			}
+
+
+            return directory_name;
+
+		}
+
+		public static bool DeletesFile(FluentFTP.FtpClient fluentSession)
+		{
+
+			System.Console.WriteLine("Please provide name of file you would like to delete");
+			var file_name = System.Console.ReadLine();
+
+            FileExists(fluentSession, file_name);
+
+            try
+            {
+                fluentSession.DeleteFile(file_name);
+                return true;
+            }
+
+            catch(WebException e){
+                
+				System.Console.WriteLine();
+				System.Console.Write(e.Message);
+
+            }
+
+            return false;
+		}
+        
+        
+        
         public static void GetFile(FluentFTP.FtpClient session, String local, String remote)
         {
             try
